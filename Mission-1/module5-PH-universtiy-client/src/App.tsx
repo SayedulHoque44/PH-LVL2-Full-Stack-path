ec889f5b73b34534cbeb0f4bbb547c7e8d152a0a
sayedulhoque44 <sayedulhoque3544@gmail.com>
2024-01-20 13:28:18 +0600

    module 6 finished

--- Mission-1/module5-PH-universtiy-client/src/App.tsx
+++ Mission-1/module5-PH-universtiy-client/src/App.tsx
@@ -1,10 +1,11 @@
 import MainLayout from "./components/layout/MainLayout";
+import ProtectedRoute from "./components/layout/protectedRoute";
 
 function App() {
   return (
-    <>
+    <ProtectedRoute>
       <MainLayout />
-    </>
+    </ProtectedRoute>
   );
 }
 
--- Mission-1/module5-PH-universtiy-client/src/redux/store.ts
+++ Mission-1/module5-PH-universtiy-client/src/redux/store.ts
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "redux-persist";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { baseApi } from "./api/baseApi";
 import authReducer from "./features/auth/authSlice";
@@ -18,12 +27,17 @@ export const store = configureStore({
     [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-//
+// persist
 export const persistor = persistStore(store);
+//
